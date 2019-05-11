9681ed8a8948b5d55546b51794abe1029b5fce48
Karo Zakaryan <karo.zakaryan.y@tumo.org>
2019-05-11 12:27:07 +0400

    test

---
+++ Lesson_4/script.js
@@ -0,0 +1,75 @@
+/*var clickCount = 0;
+function clickHandler(evt){
+   clickCount++;
+   console.log(evt);
+   var str = "Thanks for clicking " + clickCount;
+   this.innerText = str;
+}
+
+var p = document.getElementById("pElement");
+p.addEventListener("click", clickHandler);
+
+function bodyClick(evt){
+    console.log("Clicked at X: " + evt.pageX + ", Y: " + evt.pageY);
+ }
+ window.onclick = bodyClick
+ function bodyClick(evt){
+    console.log("Clicked at X: " + evt.pageX + ", Y: " + evt.pageY);
+ }
+ window.onclick = bodyClick;
+ function bodyClick(evt){
+    console.log("Clicked at X: " + evt.pageX + ", Y: " + evt.pageY);
+ }
+ window.onclick = bodyClick;
+ function bodyClick(evt){
+    console.log("Clicked at X: " + evt.pageX + ", Y: " + evt.pageY);
+ }
+ window.onclick = bodyClick;
+ function click(evt) {
+    console.log(evt.pageX, evt.pageY);
+ }
+ window.onclick = click;
+ function mousePressed() {
+    console.log(mouseX, mouseY);
+ }
+ function keydown(evt) {
+    console.log("You printed " + evt.key);
+ }
+ window.onkeydown = keydown;
+ function setup(){}
+ function keyPressed() {
+    console.log(key);
+ }*/
+ function main() {
+    var socket = io();
+    var chatDiv = document.getElementById('chat');
+    var input = document.getElementById('message');
+    var button = document.getElementById('submit');
+ 
+    function handleSubmit(evt) {
+        var val = input.value;
+        if (val != "") {
+            socket.emit("send message", val);
+        }
+    }
+    button.onclick = handleSubmit;
+    function handleMessage(msg) {
+        var p = document.createElement('p');
+        p.innerText = msg;
+        chatDiv.appendChild(p);
+        input.value = "";
+}
+
+socket.on('display message', handleMessage);
+} // main closing bracket
+
+window.onload = main;   
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
